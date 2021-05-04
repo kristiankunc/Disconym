f3adafc3b1c622cd4eece6b31397d6b651ba4dfe
KristN <kristian.kunc@gmail.com>
2021-05-04 20:09:30 +0200

    Added ping command

---
+++ cogs/ping.py
@@ -0,0 +1,19 @@
+import discord
+import json
+from pathlib import Path
+from discord.ext import commands
+
+class Ping(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        embed=discord.Embed(color=0x08ccfd)
+        embed.add_field(name="Pong", value=f"Latency is {round(self.client.latency * 1000)}ms", inline=False)
+        embed.set_footer(text=self.client.user.name)
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Ping(client))
