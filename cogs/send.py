2082ea729882898ac7903bfcc8643ce5d3172747
KristN <kristian.kunc@gmail.com>
2021-05-05 20:41:49 +0200

    Added reactions for reports

--- cogs/send.py
+++ cogs/send.py
@@ -16,23 +16,17 @@ class Send(commands.Cog):
             data_folder = Path("database/")
             file_to_open = data_folder / "blacklist.json"
 
-            with open(file_to_open) as f:
-                print(f.read())
-                blacklist = json.load(f)
-                try:
-                    blacklist_user = blacklist[str(ctx.message.author)]
-                except:
-                    pass
-
             target_dm = target.dm_channel
             if target_dm is None:
                 target_dm = await target.create_dm()
 
             embed=discord.Embed(title="New Disconym message", description=input_message, color=0x169cdf)
             embed.set_footer(text="Sent using Disconym - Anynymous Discord messanger")
-            await target_dm.send(embed=embed)
+            send_msg = await target_dm.send(embed=embed)
 
             await ctx.send(f"Message to {target.mention} has been delivered succesfully ")
+
+            await send_msg.add_reaction("❌")
 
 
 def setup(client):
