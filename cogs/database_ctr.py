f99a5b6ffecfa2abaf9c6eaa0fa58372da63da79
KristN <kristian.kunc@gmail.com>
2021-05-07 20:54:31 +0200

    Revamped database control

--- cogs/database_ctr.py
+++ cogs/database_ctr.py
@@ -1,8 +1,8 @@
 import discord
-import db_actions
-import mysql.connector as mysql
+import asyncio
 from discord.ext import commands
 from db_actions import Database
+from discord.utils import get
 
 class Database_ctr(commands.Cog):
 
@@ -10,35 +10,143 @@ class Database_ctr(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def add_prefix(self, ctx, guild_id, prefix):
-        Database.add_prefix(guild_id, prefix)
+    async def db(self, ctx):
+        author = ctx.message.author
+        pfp = author.avatar_url
 
-        await ctx.send(f"Prefix added\nGuild ID - {guild_id}\nPrefix - {prefix}")
+        emoji_one = "1️⃣"
+        emoji_two = "2️⃣"
+        emoji_three = "3️⃣"
+        emoji_four = "4️⃣"
+        emoji_five = "5️⃣"
 
-    @commands.command()
-    async def remove_prefix(self, ctx, guild_id):
-        Database.remove_prefix(guild_id)
+        if ctx.author.id == 289411795423199232:
+                
+            channel = ctx.channel
 
-        await ctx.send(f"Prefix removed\nGuild ID - {guild_id}")
+            def define_embed(title, desc):
+                embed=discord.Embed(title=title, description=desc, color=0xf49b01)
+                embed.set_footer(text=f"Executed by {ctx.author.name}", icon_url=pfp)
+                return embed
 
-    @commands.command()
-    async def replace_prefix(self, ctx, guild_id, prefix):
-        Database.replace_prefix(guild_id, prefix)
+            embed = await ctx.send(embed = define_embed("Chose category", f"{emoji_one} - Prefix\n{emoji_two} - Blacklist"))
+            await embed.add_reaction(emoji_one)
+            await embed.add_reaction(emoji_two)
 
-        await ctx.send(f"Prefix replaced\nGuild ID - {guild_id}\nPrefix - {prefix}")
+            def check(reaction, user):
+                return user == ctx.author
 
-    
-    @commands.command()
-    async def add_blacklist(self, ctx, user_id, *, reason):
-        Database.add_blacklist(user_id, reason)
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', timeout=15.0, check=check)
+            except asyncio.TimeoutError:
+                pass
+            else:
+                await embed.clear_reactions()
+                if str(reaction.emoji) == emoji_one:
+                    # PREFIX
 
-        await ctx.send(f"User {user_id} has been blacklisted")
+                    await embed.edit(embed = define_embed("Chose action for Prefix", f"{emoji_one} - add prefix\n{emoji_two} - remove prefix\n{emoji_three} - replace prefix"))
+                    await embed.add_reaction(emoji_one)
+                    await embed.add_reaction(emoji_two)
+                    await embed.add_reaction(emoji_three)
 
-    @commands.command()
-    async def remove_blacklist(self, ctx, user_id):
-        Database.remove_blacklist(user_id)
+                    try:
+                        reaction, user = await self.client.wait_for('reaction_add', timeout=15.0, check=check)
+                    except asyncio.TimeoutError:
+                        pass
+                    else:
+                        await embed.clear_reactions()
+                        if str(reaction.emoji) == emoji_one:
+                            # ----- ADD PREFIX -----
 
-        await ctx.send(f"User {user_id} has been removed from the blacklist")
+                            await embed.edit(embed = define_embed("Please send the guild/server ID", f"Example - `174837853778345984`"))
+
+                            guild_id_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await guild_id_msg.delete()
+
+                            await embed.edit(embed = define_embed("Please send the prefix", f"Example - `!`"))
+
+                            prefix_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await prefix_msg.delete()
+
+                            Database.add_prefix(guild_id_msg.content, prefix_msg.content)
+
+                            await embed.edit(embed = define_embed("Prefix set", f"Guild ID - `{guild_id_msg.content}`\nPrefix - `{prefix_msg.content}`"))
+
+
+                        elif str(reaction.emoji) == emoji_two:
+                            # ----- REMOVE PREFIX -----
+
+                            await embed.edit(embed = define_embed("Please send the guild/server ID", f"Example - `174837853778345984`"))
+
+                            guild_id_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await guild_id_msg.delete()
+
+                            Database.remove_prefix(guild_id_msg.content)
+
+                            await embed.edit(embed = define_embed("Prefix removed", f"Guild ID - `{guild_id_msg.content}`"))
+
+
+                        elif str(reaction.emoji) == emoji_three:
+                            # ----- REPLACE PREFIX -----
+
+                            await embed.edit(embed = define_embed("Please send the guild/server ID", f"Example - `174837853778345984`"))
+
+                            guild_id_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await guild_id_msg.delete()
+
+                            await embed.edit(embed = define_embed("Please send the prefix", f"Example - `!`"))
+
+                            prefix_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await prefix_msg.delete()
+
+                            Database.replace_prefix(guild_id_msg.content, prefix_msg.content)
+
+                            await embed.edit(embed = define_embed("Prefix replaced", f"Guild ID - `{guild_id_msg.content}`\nPrefix - `{prefix_msg.content}`"))
+
+                elif str(reaction.emoji) == emoji_two:
+                    # ---------- BLACKLIST ----------
+                    
+                    await embed.edit(embed = define_embed("Chose action for Blacklist", f"{emoji_one} - Add user to blacklist\n{emoji_two} - Remove user from blacklist"))
+
+                    await embed.add_reaction(emoji_one)
+                    await embed.add_reaction(emoji_two)
+
+                    try:
+                        reaction, user = await self.client.wait_for('reaction_add', timeout=15.0, check=check)
+                    except asyncio.TimeoutError:
+                        pass
+                    else:
+                        await embed.clear_reactions()
+                        if str(reaction.emoji) == emoji_one:
+                            # ----- ADD TO BLACKLIST -----
+
+                            await embed.edit(embed = define_embed("Please send the user ID", f"Example - `289411795423199232`"))
+
+                            user_id_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await user_id_msg.delete()
+
+                            await embed.edit(embed = define_embed("Please send the blacklist reason", f"Example - `harassment`"))
+
+                            reason_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await reason_msg.delete()
+
+                            Database.add_blacklist(user_id_msg.content, reason_msg.content)
+
+                            await embed.edit(embed = define_embed("User added to blacklist", f"User ID - `{user_id_msg.content}`\nUser profile - <@{user_id_msg.content}>\nBlacklist reason - `{reason_msg.content}`"))
+
+
+                        elif str(reaction.emoji) == emoji_two:
+                            # ----- REMOVE FROM BLACKLIST -----
+
+                            await embed.edit(embed = define_embed("Please send the user ID", f"Example - `289411795423199232`"))
+
+                            user_id_msg = await self.client.wait_for("message", check=lambda m:m.author==ctx.author and m.channel.id==ctx.channel.id)
+                            await user_id_msg.delete()
+
+                            Database.remove_blacklist(user_id_msg.content)
+                            await embed.edit(embed = define_embed("User removed from blacklist", f"User ID - `{user_id_msg.content}`\nUser profile - <@{user_id_msg.content}>"))
+
 
 def setup(client):
     client.add_cog(Database_ctr(client))
