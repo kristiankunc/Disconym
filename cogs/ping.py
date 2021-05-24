5b190b1a978c5a7dc5c0953e64c44834de121c39
KristN <kristian.kunc@gmail.com>
2021-05-24 23:47:22 +0200

    Added testing slash command to ping

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,11 +1,16 @@
 import discord
 import datetime
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
 
 class Ping(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    @cog_ext.cog_slash(name="ping", description="Show the bot's latency")
+    async def _ping(self, ctx: SlashContext):
+        await self.ping(ctx)
 
     @commands.command()
     async def ping(self, ctx):
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import discord
 import os
-from pathlib import Path
 from discord.ext import commands
-import mysql.connector as mysql
 from db_actions import Database
+from discord_slash import SlashCommand
 
 intents = discord.Intents.default()
 intents.members = True
@@ -15,18 +14,13 @@ def get_prefix(client, message):
         return Database.find_prefix(message.guild.id)
 
 client = commands.Bot(command_prefix= (get_prefix), intents=intents, case_insensitive=True)
+slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
 
 client.remove_command('help')
 
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
-
-
-@client.event
-async def on_command_error(ctx, error):
-    await ctx.send(f"ERROR - `{error}`")
-
 
 with open("token.txt","r") as f:
     token = f.read()
