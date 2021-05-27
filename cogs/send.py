3e306c194ac04c63fbafa97932c5f422aaacd801
KristN <kristian.kunc@gmail.com>
2021-05-27 17:34:12 +0200

    Added slash command for send

--- cogs/send.py
+++ cogs/send.py
@@ -1,9 +1,10 @@
 import discord
 import asyncio
 import datetime
-from discord import user
 from discord.ext import commands
 from db_actions import Database
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
 
 user_cache = []
 
@@ -12,17 +13,41 @@ class Send(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @cog_ext.cog_slash  (name="send",
+                        description="Send an anonymous message to desired user",
+                        options=[
+                            create_option(
+                            name="recipient",
+                            description="The user who is going to receive the message (Username#tag)",
+                            option_type=6,
+                            required=True
+                            ),
+
+                            create_option(
+                            name="message",
+                            description="The actuall message",
+                            option_type=3,
+                            required=True
+                            )
+                        ])
+
+    async def _send(self, ctx: SlashContext, recipient: discord.Member, message: str):
+        await self.send_command(ctx=ctx, target=recipient, input_message=message)
+
     @commands.command()
     @commands.dm_only()
     async def send(self, ctx, target: discord.Member, *, input_message):
+        await self.send_command(ctx, target, input_message)
+
+    async def send_command(self, ctx, target: discord.Member, *, input_message):
         if ctx.author.id in user_cache:
-            await ctx.send("Please wait before sending another message.")
+            await ctx.send("Please wait before sending another message.", hidden=True)
 
         elif ctx.author == target:
-            await ctx.send("You can't send messages to yourself :(")
+            await ctx.send("You can't send messages to yourself :(", hidden=True)
 
         elif input_message == None:
-            await ctx.send("You can not send an empty message")
+            await ctx.send("You can not send an empty message", hidden=True)
                 
         else:
             user_cache.append(ctx.author.id)
@@ -49,13 +74,13 @@ class Send(commands.Cog):
 
                     await log_msg.edit(embed=embed)
 
-                    await ctx.send(f"Message to {target.mention} has been delivered succesfully ")
+                    await ctx.send(f"Message to {target.mention} has been delivered succesfully", hidden=True)
 
                     await asyncio.sleep(60)
                     user_cache.remove(ctx.author.id)
 
             else:
-                await ctx.send("Error, you are blacklisted from sending Disconym messages")
+                await ctx.send("Error, you are blacklisted from sending Disconym messages", hidden=True)
 
 def setup(client):
     client.add_cog(Send(client))
