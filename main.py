3dc6441e77a02ece5ce5026f1f14380d54f3d779
KristN <kristian.kunc@gmail.com>
2021-06-01 13:17:13 +0200

    Changed cog loading

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
 
 client.remove_command('help')
 
-for filename in os.listdir("./cogs"):
+for filename in os.listdir("./commands"):
     if filename.endswith(".py"):
-        client.load_extension(f"cogs.{filename[:-3]}")
+        client.load_extension(f"commands.{filename[:-3]}")
+
+for filename in os.listdir("./functions"):
+    if filename.endswith(".py"):
+        client.load_extension(f"functions.{filename[:-3]}")
 
 with open("token.txt","r") as f:
     token = f.read()
