c028353e78e4f3878ac652d5903de425bce1a2a1
KristN <kristian.kunc@gmail.com>
2021-06-10 19:17:43 +0200

    Added (somewhat) slash commands error handling

--- functions/errors.py
+++ functions/errors.py
@@ -1,6 +1,8 @@
+from typing import AsyncIterable
 import discord
 import difflib
 import datetime
+from discord import embeds
 from discord.ext.commands.errors import MissingPermissions
 from db_actions import Database
 from discord.ext import commands
@@ -10,19 +12,30 @@ class Errors(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        command_list = []
+    async def handle_error(self, ctx, error, slash):
+        if slash == True:
+            if ctx.guild_id != None:
+                prefix = Database.find_prefix(ctx.guild.id)[0]
+            else:
+                prefix = "!"
 
-        if ctx.guild != None:
-            prefix = Database.find_prefix(ctx.guild.id)[0]
-        else:
-            prefix = "!"
+            author = self.client.get_user(ctx.author_id)
+            channel = self.client.get_channel(ctx.channel_id)
+            command_name = ctx.name
 
-        command_name = ctx.message.content.split(' ')[0].replace(prefix, '')
+        elif slash == False:
+            if ctx.guild != None:
+                prefix = Database.find_prefix(ctx.guild.id)[0]
+            else:
+                prefix = "!"
 
-        for command in self.client.commands:
-            command_list.append(command.name)
+            author = ctx.author
+            channel = ctx.channel
+            command_name = ctx.message.content.split(' ')[0].replace(prefix, '')
+
+        print(author)
+        print(channel)
+        print(command_name)
 
         def genEmbed(error_response):
             embed=discord.Embed(title="Error", description=error_response, color=0xff1f1f)
@@ -32,7 +45,7 @@ class Errors(commands.Cog):
 
         def suggestCommand(user_input):
             try:
-                return f"Did you mean `{prefix}{difflib.get_close_matches(user_input, command_list)[0]}`?"
+                return f"Did you mean `{prefix}{difflib.get_close_matches(user_input, self.client.commands)[0]}`?"
             except:
                 return f"You can try `{prefix}help`"
 
@@ -50,6 +63,14 @@ class Errors(commands.Cog):
         else:
             print("-------------------")
             print(error)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await self.handle_error(ctx, error, False)
+
+    @commands.Cog.listener()
+    async def on_slash_command_error(self, ctx, error):
+        await self.handle_error(ctx, error, True)
 
 def setup(client):
     client.add_cog(Errors(client))
