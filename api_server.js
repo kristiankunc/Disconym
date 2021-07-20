db467b0fca33af0c17c895ca082abf9de9fd39e2
Kristian <kristian.kunc@gmail.com>
2021-07-21 01:22:47 +0200

    changed port to 80

--- api_server.js
+++ api_server.js
@@ -4,7 +4,7 @@ const { json } = require("express");
 
 const app = express();
 
-const port = 3000;
+const port = 80;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 async function get_api(){
