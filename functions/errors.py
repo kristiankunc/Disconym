1269cf17b5fc2f3472490cc7a9b77da98bfb193e
KristN <kristian.kunc@gmail.com>
2021-08-10 19:19:46 +0200

    added UserNotFound handler

--- functions/errors.py
+++ functions/errors.py
@@ -13,6 +13,9 @@ class Errors(commands.Cog):
         self.client = client
 
     async def handle_error(self, ctx, error, slash):
+        bot_name = self.client.user.name
+        bot_pfp = self.client.user.avatar_url
+
         if slash == True:
             if ctx.guild_id != None:
                 prefix = Database.find_prefix(ctx.guild.id)[0]
@@ -29,25 +32,23 @@ class Errors(commands.Cog):
             else:
                 prefix = "!"
 
-            author = ctx.author
-            channel = ctx.channel
             command_name = ctx.message.content.split(' ')[0].replace(prefix, '')
-
-        print(author)
-        print(channel)
-        print(command_name)
 
         def genEmbed(error_response):
             embed=discord.Embed(title="Error", description=error_response, color=0xff1f1f)
-            embed.set_footer(text=f"©️ {self.client.user.name}")
+            embed.set_footer(text=bot_name, icon_url=bot_pfp)
             embed.timestamp = datetime.datetime.now()
             return embed
 
         def suggestCommand(user_input):
+            available_commands = []
+            for command in self.client.commands:
+                available_commands.append(command.name)
+
             try:
-                return f"Did you mean `{prefix}{difflib.get_close_matches(user_input, self.client.commands)[0]}`?"
+                return f"Did you mean `{prefix}{difflib.get_close_matches(user_input, available_commands)[0]}`?"
             except:
-                return f"You can try `{prefix}help`"
+                return f"You can do `{prefix}help` to see all the available commands"
 
         if isinstance(error, commands.CommandNotFound):
             await ctx.send(embed=genEmbed(f"Command **{command_name}** not found!\n{suggestCommand(command_name)}"))
@@ -58,7 +59,9 @@ class Errors(commands.Cog):
         elif isinstance(error, MissingPermissions):
             await ctx.send(embed=genEmbed(f"You are missing permissions to run **{command_name}**"))
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.send(embed=genEmbed(f"Member **{ctx.message.content.split(' ')[1]}** not found"))
+            await ctx.send(embed=genEmbed(f"Member not found"))
+        elif isinstance(error, commands.UserNotFound):
+            await ctx.send(embed=genEmbed(f"User not found"))
 
         else:
             print("-------------------")
